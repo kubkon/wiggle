cce3e853da473c76027a11585118d33683a3055f
Jakub Konka <jakub.konka@golem.network>
2020-02-03 21:02:16 +0100

    Refactor overlaps method of Region struct
    This commit refactors `Region::overlaps` method.

--- crates/memory/src/region.rs
+++ crates/memory/src/region.rs
@@ -16,27 +16,11 @@ impl Region {
         let rhs_start = rhs.start as u64;
         let rhs_end = rhs_start + (rhs.len - 1) as u64;
 
-        // start of rhs inside self:
-        if rhs_start >= self_start && rhs_start < self_end {
-            return true;
+        if self_start <= rhs_start {
+            self_end >= rhs_start
+        } else {
+            rhs_end >= self_start
         }
-
-        // end of rhs inside self:
-        if rhs_end >= self_start && rhs_end < self_end {
-            return true;
-        }
-
-        // start of self inside rhs:
-        if self_start >= rhs_start && self_start < rhs_end {
-            return true;
-        }
-
-        // end of self inside rhs: XXX is this redundant? i suspect it is but im too tired
-        if self_end >= rhs_start && self_end < rhs_end {
-            return true;
-        }
-
-        false
     }
 }
 
